22b56687282d7da5733a6317fb4629e6a915b9e9
Damian Hites <Nizebulous@gmail.com>
2014-11-11 21:26:24 -0800

    Initial pass of a more styled homepage

--- helpers.rb
+++ helpers.rb
@@ -2,4 +2,15 @@ helpers do
   def image_tag(source)
     "<img src='#{source}'/>"
   end
+
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||= Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+  end
 end
